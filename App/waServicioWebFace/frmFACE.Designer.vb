73e21b7e2a615b5e2511bdc936d7786b6fbe61d9
Josep Maria Herrando <josepmaria@neuronasistemas.com>
2015-02-19 16:52:18 +0100

    App VB.NET
    Para enviar factura .xsig utilizando certificado .pfx

---
+++ App/waServicioWebFace/frmFACE.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFACE
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnConectarFace = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnConectarFace
+        '
+        Me.btnConectarFace.Location = New System.Drawing.Point(67, 35)
+        Me.btnConectarFace.Name = "btnConectarFace"
+        Me.btnConectarFace.Size = New System.Drawing.Size(149, 48)
+        Me.btnConectarFace.TabIndex = 0
+        Me.btnConectarFace.Text = "Conectar con Face"
+        Me.btnConectarFace.UseVisualStyleBackColor = True
+        '
+        'frmFACE
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 128)
+        Me.Controls.Add(Me.btnConectarFace)
+        Me.Name = "frmFACE"
+        Me.Text = "Face WebService"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnConectarFace As System.Windows.Forms.Button
+
+End Class
---
+++ App/waServicioWebFace/frmFACE.vb
@@ -0,0 +1,72 @@
+﻿Imports Microsoft.Web.Services3
+Imports Microsoft.Web.Services3.Security
+Imports Microsoft.Web.Services3.Security.Tokens
+Imports Microsoft.Web.Services3.Security.X509
+Imports Microsoft.Web.Services3.Design
+Imports System.Security.Cryptography
+Public Class frmFACE
+    Dim cert As New System.Security.Cryptography.X509Certificates.X509Certificate2("C:\DIRECTORIOCERTIFICADO\NOMBRECERTIFICADO.pfx", "PASSWORDCERTIFICADO")
+    Private Sub frmFACE_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnConectarFace_Click(sender As Object, e As EventArgs) Handles btnConectarFace.Click
+        Dim webserviceProxy As New facewebservice.SSPPWebServiceProxyService
+        webserviceProxy.ClientCertificates.Add(cert)
+        Dim factura As New facewebservice.SSPPFactura
+        Dim signatureToken As X509SecurityToken = GetSecurityToken()
+        Dim requestContext As SoapContext = webserviceProxy.RequestSoapContext
+
+        requestContext.Security.Tokens.Add(signatureToken)
+        Dim sig As New MessageSignature(signatureToken)
+        requestContext.Security.Timestamp.TtlInSeconds = 60
+        requestContext.Security.Elements.Add(sig)
+
+        Dim fra As New facewebservice.SSPPFicheroFactura()
+
+        factura.correo = "direccion@email.com"
+
+        Dim line As String = ""
+        Try
+
+            Using sr As New System.IO.StreamReader("C:\DIRECTORIO_DE_LA_FACTURA\NOMBREDELAFACTURA.xsig")
+
+                line = sr.ReadToEnd()
+                Console.WriteLine(line)
+            End Using
+        Catch ex As Exception
+            Console.WriteLine("The file could not be read:")
+            Console.WriteLine(ex.Message)
+        End Try
+
+
+        fra.factura = line
+        fra.nombre = "NOMBREDELAFACTURA.xsig"
+        fra.mime = "application/xml"
+
+        factura.fichero_factura = fra
+
+        Try
+
+            Dim resultadoEnvioFactura As New facewebservice.SSPPResultadoEnviarFactura
+            resultadoEnvioFactura = webserviceProxy.enviarFactura(factura)
+            MsgBox(resultadoEnvioFactura.codigo_registro)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Public Function GetSecurityToken() As X509SecurityToken
+        Dim securityToken As X509SecurityToken = Nothing
+
+        Try
+            securityToken = New X509SecurityToken(cert)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Return securityToken
+    End Function
+
+End Class
